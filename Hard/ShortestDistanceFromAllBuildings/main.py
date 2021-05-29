325bf4a54bfa38ddc2797f57f7971f890f75234b
Kid Vader <nickjamil@gmail.com>
2021-05-29 18:55:50 -0400

    Fix a misunderstanding in the requirements of the Block problem

--- Hard/ShortestDistanceFromAllBuildings/main.py
+++ Hard/ShortestDistanceFromAllBuildings/main.py
@@ -17,17 +17,17 @@ def get_best_block(blocks, requirements):
     keys of the block facilities. Ex:
     ['gym', 'school', 'store']
 
-    :returns: The index of the city block that minimizes the distance to all the 
-    required facilities. Ex:
-    2, 3, or 4 are all valid answers, because they each minimize total distance for 
-    all required facilities to 2 blocks.
+    :returns: The index of the city block that minimizes the max distance need to visit
+    a required facility. Ex:
+    3, because the school is there so distance is 0; and gym and store are each only
+    1 block away.
 
     O(n^2 x m) where n is the number of blocks and m is the number of required
     facilities.
     '''
     distances = [get_requirement_distances_from_block(blocks, requirements, i) for i, _ in enumerate(blocks)]
-    distance_sums = get_sums(distances)
-    min_distance_block = get_index_of_min(distance_sums)
+    distance_maxes = get_maxes(distances)
+    min_distance_block = get_index_of_min(distance_maxes)
 
     return min_distance_block
 
@@ -51,9 +51,9 @@ def get_requirement_distances_from_block(blocks, requirements, current_block):
     :param `current_block`: The index of the current block in the blocks array.
     Ex: 2
 
-    :returns: A map of facilities --> the min distance to that facility from the
-    current_block. Ex.
-    {'gym': 0, 'school': 0, 'Store': 2,}
+    :returns: A map of facilities --> the nearest distance to that facility from the
+    `current_block`. Ex.
+    {'gym': 0, 'school': 0, 'store': 2,}
     '''
     facilities = { facility: math.inf for facility in requirements }
 
@@ -67,29 +67,29 @@ def get_requirement_distances_from_block(blocks, requirements, current_block):
     return facilities
 
 
-def get_sums(distances):
+def get_maxes(distances):
     '''
     :param `distances`: An array of blocks, each containing the distance to the nearest
     facility for a specific set of facilities. Ex.
     [
-        {'gym': 1, 'school': 0, 'Store': 4,},
-        {'gym': 0, 'school': 1, 'Store': 3,},
-        {'gym': 0, 'school': 0, 'Store': 2,},
-        {'gym': 1, 'school': 0, 'Store': 1,},
-        {'gym': 2, 'school': 0, 'Store': 0,},
+        {'gym': 1, 'school': 0, 'store': 4,},
+        {'gym': 0, 'school': 1, 'store': 3,},
+        {'gym': 0, 'school': 0, 'store': 2,},
+        {'gym': 1, 'school': 0, 'store': 1,},
+        {'gym': 2, 'school': 0, 'store': 0,},
     ]
 
-    :returns: An array of blocks, each containing the total distance to all specified
+    :returns: An array of blocks, each containing the max distance of all specified
     facilities. Ex:
-    [5, 4, 2, 2, 2]
+    [4, 3, 2, 1, 2]
     '''
-    return [sum(block.values()) for block in distances]
+    return [max(block.values()) for block in distances]
 
 
 def get_index_of_min(list):
     '''
     :returns: The index of the minimum element of the int array. Ex:
-    [5, 4, 2, 2, 2] --> 2 or 3 or 4
+    [4, 3, 2, 1, 2] --> 3
     '''
     return list.index(min(list))
 
